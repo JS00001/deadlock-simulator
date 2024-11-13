90d0165424ddad94293749cdef3c95ee24debe89
JS <49812749+JS00001@users.noreply.github.com>
2024-11-13 18:08:24 -0500

    update simulation

--- src/simulations/simulation_3.py
+++ src/simulations/simulation_3.py
@@ -8,14 +8,21 @@ def simulation_3():
     detector.allocate_resource(2, 2)
     detector.allocate_resource(3, 3)
     detector.allocate_resource(4, 4)
+    detector.allocate_resource(5, 5)
 
     # Request resources
     detector.request_resource(1, 2)
     detector.request_resource(2, 3)
     detector.request_resource(3, 4)
     detector.request_resource(4, 1)
+
+    detector.request_resource(5, 1) 
+    detector.request_resource(1, 5)  
+    detector.request_resource(3, 2) 
     
     # Deadlock State: 1 is waiting for 2, 2 is waiting for 3, 3 is waiting for 4, 4 is waiting for 1
+    # Also, 5 is waiting for 1, 1 is waiting for 5, 3 is waiting for 2
+    # Therefore, we need to resolve the deadlock by releasing process 2's resource
     deadlocked = detector.detect_deadlock()
 
     if deadlocked:
